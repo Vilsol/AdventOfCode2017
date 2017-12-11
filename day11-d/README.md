20b5219d4e578ee070e044ae24f03e7138bcbe4b
Vilsol <vilsol2000@gmail.com>
2017-12-12 00:25:51 +0200

    Day 11: D

---
+++ day11-d/README.md
@@ -0,0 +1,9 @@
+# Day 11 - D
+
+**Time taken:** 1 hour
+
+**[Experience level](https://github.com/Vilsol/AdventOfCode2017/blob/master/README.md#experience-levels):** 0
+
+## Resources used:
+
+* https://dlang.org/phobos/index.html
---
+++ day11-d/day11-1.d
@@ -0,0 +1,53 @@
+module day1;
+
+import std.range, std.stdio;
+import std.regex: split, regex;
+import std.math: abs;
+
+void main()
+{
+    auto file = File("input.txt");
+    auto range = file.byLineCopy();
+    auto firstLine = range.takeExactly(1).front;
+    auto s = split(firstLine, regex(",", "g"));
+
+    auto x = 0.0;
+    auto y = 0.0;
+
+    foreach(command; s){
+        switch(command){
+            case "n":
+                y += 1;
+                break;
+            case "s":
+                y -= 1;
+                break;
+            case "e":
+                x += 1;
+                break;
+            case "w":
+                x -= 1;
+                break;
+            case "nw":
+                x -= 0.5;
+                y += 0.5;
+                break;
+            case "ne":
+                x += 0.5;
+                y += 0.5;
+                break;
+            case "sw":
+                x -= 0.5;
+                y -= 0.5;
+                break;
+            case "se":
+                x += 0.5;
+                y -= 0.5;
+                break;
+            default:
+                break;
+        }
+    }
+
+    writeln(abs(x) + abs(y));
+}
